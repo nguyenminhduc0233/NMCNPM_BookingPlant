5570584b828a9301d9b7a3173a26ecda8791d4c7
Nguyễn Minh Đức <114336100+nguyenminhduc0233@users.noreply.github.com>
2023-06-14 18:40:21 +0700

    Add files via upload

---
+++ banners.sql
@@ -0,0 +1,2 @@
+INSERT INTO `banners` (`id_banner`, `img`, `allow`, `discount`, `content`) VALUES (1, 'img/slideshow_1_600x.jpg', '1', 0.02, 'Nhanh tay đặt hàng');
+INSERT INTO `banners` (`id_banner`, `img`, `allow`, `discount`, `content`) VALUES (2, 'img/1011890.jpg', '1', 0.08, 'Số lượng có hạn');
